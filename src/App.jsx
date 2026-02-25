27164ab95f18c12918b311a21e2ef88b2f37c4b5
Amisha Chaubey <amishaaryani1@gmail.com>
2026-02-25 20:06:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import IconicTower from './page/Home'
+
+const App = () => {
+  return (
+    <div>
+      <IconicTower/>
+    </div>
+  )
+}
+
+export default App
